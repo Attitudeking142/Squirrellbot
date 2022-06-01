fdc4b794e431b6fa5dd7cfa976c60b35e7d83668
Attitudeking142 <105148300+Attitudeking142@users.noreply.github.com>
2022-06-01 02:43:31 -0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Attitudequeen143/squirrellbot",
+    "https://github.com/Attitudeking142/Squirrellbot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
